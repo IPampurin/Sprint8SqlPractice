49a9b39ace446385dfb67045048bcfa9fc2441fb
IPampurin <i.pampurin@yandex.ru>
2025-01-26 17:48:34 +0500

    task ready

--- etalonMain.go
+++ etalonMain.go
@@ -16,36 +16,33 @@ type Sale struct {
 // String реализует метод интерфейса fmt.Stringer для Sale, возвращает строковое представление объекта Sale.
 // Теперь, если передать объект Sale в fmt.Println(), то выведется строка, которую вернёт эта функция.
 func (s Sale) String() string {
-	return fmt.Sprintf("Product: %d, Volume: %d, Date: %s", s.Product, s.Volume, s.Date)
+	return fmt.Sprintf("Product: %d Volume: %d Date:%s", s.Product, s.Volume, s.Date)
 }
 
 func selectSales(client int) ([]Sale, error) {
-	var sales []Sale
-
 	db, err := sql.Open("sqlite", "demo.db")
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT product, volume, date FROM sales WHERE id = :id", sql.Named("id", client))
+	var sales []Sale
+
+	rows, err := db.Query("SELECT product, volume, date FROM sales WHERE client = :client", sql.Named("client", client))
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		currentSale := Sale{}
+		sale := Sale{}
 
-		err := rows.Scan(&currentSale.Product, &currentSale.Volume, &currentSale.Date)
+		err := rows.Scan(&sale.Product, &sale.Volume, &sale.Date)
 		if err != nil {
 			return nil, err
 		}
-		sales = append(sales, currentSale)
-	}
 
-	if err := rows.Err(); err != nil {
-		return nil, err
+		sales = append(sales, sale)
 	}
 
 	return sales, nil
